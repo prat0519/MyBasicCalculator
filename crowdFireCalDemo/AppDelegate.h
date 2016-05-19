6c5697946bb1b97fa2280c39c9cab78c4a2d0b5c
Prashant Pandey <prat0519@prashants-macbook-pro.local>
2016-05-19 17:57:30 +0530

    Initial Commit

---
+++ crowdFireCalDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  crowdFireCalDemo
+//
+//  Created by Prashant Pandey on 19/05/16.
+//  Copyright © 2016 Prashant Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ crowdFireCalDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  crowdFireCalDemo
+//
+//  Created by Prashant Pandey on 19/05/16.
+//  Copyright © 2016 Prashant Pandey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
